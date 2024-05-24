f53b85cb019e4ec8ad3fc0793f4efef9042addc6
Abubakar <abubakarsiddikl.info@gmail.com>
2024-05-25 01:23:37 +0600

    layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -5,10 +5,10 @@ import SideBar from "../pages/sub-components/SideBar/SideBar";
 const MainLayout = () => {
   return (
     <div className="mainBody">
-      <div className="flex align-items-center px-3 border">
+      <div className="flex align-items-center px-3 border overflow-auto">
         <SideBar />
       </div>
-      <div className="border">
+      <div className="border overflow-auto position-sticky bottom-0 left-0">
         <Outlet />
       </div>
     </div>
