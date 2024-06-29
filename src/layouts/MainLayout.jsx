5cd67bf5f8a5924ce46078e0fea1eee961b248a8
Abubakar <abubakarsiddikl.info@gmail.com>
2024-06-29 12:31:00 +0600

    header

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -8,7 +8,7 @@ const MainLayout = () => {
       <div className="flex align-items-center px-3 bg-secondary overflow-auto">
         <SideBar />
       </div>
-      <div className=" overflow-auto position-sticky bottom-0 left-0">
+      <div className=" overflow-auto position-sticky bottom-0 left-0 pe-3">
         <Outlet />
       </div>
     </div>
--- src/pages/sub-components/Header.jsx
+++ src/pages/sub-components/Header.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+import { LuSearchCode } from "react-icons/lu";
+import { MdEditLocationAlt, MdOutlineEditLocationAlt } from "react-icons/md";
+
 const Header = () => {
+  const [isClicked, setIsClicked] = useState(false);
   return (
-    <div>
-      <h1>header</h1>
+    <div
+      className="d-flex align-items-center justify-content-between w-100 my-2"
+      style={{ color: "#7d7d7d" }}
+    >
+      <span style={{ fontSize: 22, fontWeight: 600 }}>BongoBOT</span>
+      <div className="d-flex align-items-center">
+        <div
+          className="px-2 py-1 rounded me-2"
+          style={{ backgroundColor: "rgba(244, 244, 244,1)", cursor: "pointer" }}
+        >
+          <LuSearchCode style={{ fontSize: 20 }} /> <span>Explore Developer</span>
+        </div>
+        {!isClicked ? (
+          <MdOutlineEditLocationAlt
+            style={{ fontSize: 25, cursor: "pointer" }}
+            onClick={() => setIsClicked(true)}
+          />
+        ) : (
+          <MdEditLocationAlt
+            style={{ fontSize: 25, cursor: "pointer" }}
+            onClick={() => setIsClicked(false)}
+          />
+        )}
+      </div>
     </div>
   );
 };
