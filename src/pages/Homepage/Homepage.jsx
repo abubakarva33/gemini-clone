2c85dcecfc3a08000994cd5a9474032a35112a11
Abubakar <abubakarsiddikl.info@gmail.com>
2024-08-06 23:48:39 +0600

    hot ques template

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -9,15 +9,20 @@ import Footer from "../sub-components/Footer";
 import Header from "../sub-components/Header";
 import CustomizeInventory from "../sub-components/CustomizeInventory/CustomizeInventory";
 
+const hotQuestions = [
+  { ques: "Describe about abubakar siddik" },
+  { ques: "who abubakar siddik" },
+  { ques: "abubakar siddik works history" },
+  { ques: "abubakar siddik personal backgraound" },
+];
+
 const Homepage = () => {
   const { id } = useParams();
   const responseContainerRef = useRef(null);
   const { history } = useSelector((state) => state.chatHistory);
   const [loading, setIsLoading] = useState(false);
   const [isCustomizeOpen, setIsCustomizeOpen] = useState(false);
-  const { sideBg,  textPrimary, } = useSelector(
-    (state) => state.customizeSec
-  );
+  const { sideBg, textPrimary } = useSelector((state) => state.customizeSec);
 
   const scrollToBottom = () => {
     if (responseContainerRef.current) {
@@ -38,6 +43,14 @@ const Homepage = () => {
       <div ref={responseContainerRef} style={{ marginBottom: 20 }} className="overflow-y-auto">
         <Response id={id} className="response " />
         <Spin spinning={loading} height={100}></Spin>
+      </div>
+
+      <div className="hot-questions">
+        {hotQuestions?.map((item, ind) => (
+          <div key={ind} style={{ border: `1px solid ${sideBg}` }}>
+            {item?.ques}
+          </div>
+        ))}
       </div>
       <div className="d-flex align-items-center flex-column" style={{ width: "80%" }}>
         <InputSection {...{ id, setIsLoading }} />
