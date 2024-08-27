f747beebdc325c3a11f3ef011e255577690b928a
Abubakar <abubakarsiddikl.info@gmail.com>
2024-08-27 23:56:59 +0600

    skills add

--- src/utilities/devInfo.js
+++ src/utilities/devInfo.js
@@ -5,6 +5,13 @@ Personal information:
 Name: Abubakar Siddik, Age: 23, Gender: male, Mairrage status: unmarraied, Girlfriend: Don’t have, Address: Tangail, Dhaka, Bangladesh.
 Education: JSC and SSC- Dhanbari Nawab Instituition, Dhanbari, Tangail passed in 2017; HSC- Dhanbari govt. Collage, Dhanbari, Tangail passed in 2019. B.Sc in Botany from Banghabandhu Sheikh Mujibur Rahman Science & Technology University- Gopalganj. Completed a web development course from Programing Hero in Bangladesh. 
 
+Skills: 
+-HTML
+-CSS
+-Javascript
+-ReactJs
+-Redux
+
 Contact Information:
 Email: abubakarsiddikl.info@gmail.com
 Number: 01580743409, 01750891998
